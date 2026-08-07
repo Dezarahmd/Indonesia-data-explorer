348433cf3c6430ac4792c389a5125062ec03bf5c
Dezar Ahmad Ramadhan <Dezarahmad@gmail.com>
2026-08-08 00:10:09 +0700

    style: navbar all done

--- src/js/app.js
+++ src/js/app.js
@@ -15,3 +15,36 @@ window.addEventListener("scroll", () => {
         isScrolled = false;
     }
 });
+
+const sections = document.querySelectorAll("main section");
+const navLinks = document.querySelectorAll(".nav-link");
+
+window.addEventListener("scroll", () => {
+
+    let current = "";
+
+    sections.forEach(section => {
+
+        const sectionTop = section.offsetTop - 120;
+        const sectionHeight = section.offsetHeight;
+
+        if (window.scrollY >= sectionTop &&
+            window.scrollY < sectionTop + sectionHeight) {
+
+            current = section.getAttribute("id");
+
+        }
+
+    });
+
+    navLinks.forEach(link => {
+
+        link.classList.remove("active");
+
+        if (link.getAttribute("href") === `#${current}`) {
+            link.classList.add("active");
+        }
+
+    });
+
+});
